fa938d98170cb965dff6ce8feb9b30c79caae0f6
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2024-04-17 11:16:16 +0200

    sec

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("new projects")
+def new_game():
+    pass
+def check_answer():
+    pass
+def display_score():
+    pass
+def play_again():
+    pass
+
+
+questions = {
+    "Who created Python? ": "A",
+    "What year was python created? ": "B",
+    "Python is tributed to which comeedy group? ": "C",
+    "Is the Earth round?": "A"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C: Bill Gates", "D. Mark Zuckerberg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. sometimes", "D. What is Earth?"]
+           ]
