3289934d2067c77f698260820dc73c5278d0f00a
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2024-04-21 13:27:21 +0200

    quiz

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 def new_game():
-    pass
-def check_answer():
-    pass
+    num_answers=1
+    right_answers=0
+    input_letter = []
+    for quest,value in questions.items():
+        print("----------------------------")
+        print("----------------------------")
+        print(quest)
+        for answer in options[num_answers-1]:
+            print(answer)
+        print("----------------------------")
+        num_answers +=1
+        lette=input("Enter the answer! ").upper()
+        input_letter.append(lette)
+        right_answers+=check_answer(lette,value)
+    print(input_letter)
+    print(f"You have {right_answers} right answers of {len(questions)} questions")
+def check_answer(entered_letter, correct_answer):
+    if entered_letter==correct_answer:
+        print("Correct!")
+        return 1
+    else:
+        print("Incorrect!")
+        return 0
 def display_score():
     pass
 def play_again():
-    pass
+    pl_ag = input("Do you want to play again? yes or no ")
+    if pl_ag=="no":
+        return False
+    else:
+        return True
 
 
 questions = {
@@ -20,3 +44,7 @@ options = [["A. Guido van Rossum", "B. Elon Musk", "C: Bill Gates", "D. Mark Zuc
            ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
            ["A. True", "B. False", "C. sometimes", "D. What is Earth?"]
            ]
+
+new_game()
+while play_again():
+    new_game()
